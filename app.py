52149415485c3763a47c0cddd232300f7317dd4b
Your Name <you@example.com>
2025-10-11 21:21:50 +0330

    initial commit

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def home():
     return send_from_directory(os.getcwd(), "index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
